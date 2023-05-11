157aa0c2510d25b4d83c79fd65ce3e2e17905ea7
g2px1 <kir1kir1@yandex.ru>
2023-05-11 21:00:08 +0300

    change type of project in CMakeLists.txt from executable to library

--- theia_zip/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "ZIPArchive.h"
-
-int main(int argc, char *argv[]) {
-    ZIPArchive archiver("/Users/kirillzhukov/Desktop/mvfst", "/Users/kirillzhukov/Desktop/ar/mvfst.zip");
-    archiver.archive();
-    return 0;
-}
-
